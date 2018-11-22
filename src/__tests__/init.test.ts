972492d020aebbf25ff79b58fc0bbd6542653fff
Joe Genereux <josephgenereuxmail@gmail.com>
2018-11-21 17:15:47 -0700

    package setup

---
+++ src/__tests__/init.test.ts
@@ -0,0 +1,5 @@
+import { init } from '../index'
+
+test('init', () => {
+    expect(init('dev')).toBe('Agent-web-shim is running for: dev')
+})
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export const init = (name: string): string => `Agent-web-shim is running for: ${name}`
