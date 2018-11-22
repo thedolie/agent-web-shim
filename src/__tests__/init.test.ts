d5b0884cc1181460c1afd2f390fba83daf02ddcc
Joe Genereux <josephgenereuxmail@gmail.com>
2018-11-21 17:17:11 -0700

    0.1.1

--- src/__tests__/init.test.ts
+++ src/__tests__/init.test.ts
@@ -1,5 +1,5 @@
-import { init } from '../index'
+import { init } from '../index';
 
 test('init', () => {
-    expect(init('dev')).toBe('Agent-web-shim is running for: dev')
-})
+  expect(init('dev')).toBe('Agent-web-shim is running for: dev');
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export const init = (name: string): string => `Agent-web-shim is running for: ${name}`
+export const init = (name: string): string => `Agent-web-shim is running for: ${name}`;
