eea60c7300182fd42276e18302e87ee316c0cec2
Joe Genereux <josephgenereuxmail@gmail.com>
2019-01-07 11:48:34 -0700

    adds pack and unpack

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
-// Agent to Agent pack and unpack web shim
+// This library provides a MOCK pack and unpack library for the agent to agent messaging
 
+interface IAgentMessage {
+    type: string,
+    packed: boolean,
+    body: any
+}
 
-export const init = (name: string): string => `Agent-web-shim is running for: ${name}`;
+interface IPackedMessage extends IAgentMessage {
+    packed: true
+}
+interface IUnPackedMessage extends IAgentMessage {
+    packed: false
+}
+
+export const pack = (message: any): IPackedMessage => { return {type: 'SSS', packed: true, body: message} }
+
+export const unpack = (message: any): IUnPackedMessage => { return {type: 'SSS', packed: false, body: message} }
